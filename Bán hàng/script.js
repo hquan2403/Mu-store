74c6116c26cd30b27a4596bbccb91c7dbcdb8622
Winhmm <5001104118@student.hcmue.edu.vn>
2024-11-24 10:37:49 +0700

    Add files via upload

---
+++ Bán hàng/script.js
@@ -0,0 +1,24 @@
+// Hàm để kiểm tra xem phần tử có nằm trong cửa sổ trình duyệt hay không
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  // Lắng nghe sự kiện cuộn trang (scroll)
+  window.addEventListener('scroll', function() {
+    const content = document.querySelector('.content');
+    
+    if (isElementInViewport(content)) {
+      content.classList.add('visible');
+    }
+  });
+  
+
+
+  
+  
